4e1721c5681c5a3a9e0007f7b02ebca22c189422
Hayato Kimura <prprhyt@outlook.com>
2016-02-12 16:11:20 +0900

    Handheld画面回転時の異常終了問題の解決 (画面を縦方向に固定)

--- Abc2016312c/mobile/src/main/java/jagsc/org/abc2016312c/MainActivity.java
+++ Abc2016312c/mobile/src/main/java/jagsc/org/abc2016312c/MainActivity.java
@@ -60,14 +60,14 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
         /*editText1 = (EditText) findViewById(R.id.editText1);
         editText2 = (EditText) findViewById(R.id.editText2);*/
 
-        Button sendBtn = (Button) findViewById(R.id.sendBtn);
+        /*Button sendBtn = (Button) findViewById(R.id.sendBtn);
         sendBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //String msg = editText1.getText().toString();
                 bluetoothTask.doSend(resultstr);
             }
-        });
+        });*/
         /*
         Button resetBtn = (Button) findViewById(R.id.resetBtn);
         resetBtn.setOnClickListener(new View.OnClickListener() {
@@ -107,14 +107,8 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
 
     @Override
     protected void onDestroy() {
-        bluetoothTask.doClose();
         super.onDestroy();
-    }
-
-    protected void restart() {
-        Intent intent = this.getIntent();
-        this.finish();
-        this.startActivity(intent);
+        bluetoothTask.doClose();
     }
 
     //----------------------------------------------------------------
